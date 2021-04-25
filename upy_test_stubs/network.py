a447a9d801f7e66cff5ddbb7d3302dca498f27fd
Davide Depau <davide@depau.eu>
2021-04-25 23:54:52 +0200

    Simulate Wi-Fi connection

--- upy_test_stubs/network.py
+++ upy_test_stubs/network.py
@@ -12,6 +12,7 @@ class WLAN:
     def __init__(self, *args):
         print(f"STUB network.WIFI create{args}")
         self.args = args
+        self.conncheck_count = 0
 
     def active(self, active=True):
         print(f"STUB network.WIFI.active({active})")
@@ -20,11 +21,12 @@ class WLAN:
     def config(self, **kwargs):
         print(f"STUB network.WIFI.config({kwargs})")
 
-    def connect(self, ssid, psk):
+    def connect(self, ssid, psk, *a, **kw):
         print(f"STUB network.WIFI.connect('{ssid}', '{psk}')")
 
     def isconnected(self):
-        return True
+        self.conncheck_count += 1
+        return self.conncheck_count > 10
 
     def scan(self):
         print(f"STUB network.WIFI.scan()")
