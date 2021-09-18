dac5a6c50af8c7602ab0827fd0c8a04847b53c9d
Andrea Brancaleoni <abc@pompel.me>
2021-09-18 10:34:05 +0200

    *: log every exception

--- _boot.py
+++ _boot.py
@@ -24,6 +24,15 @@ try:
 except ImportError:
     pass
 
+# Log every exceptions
+from leviot import ulog
+log = ulog.Logger("exceptions")
+import sys
+def my_except_hook(exctype, value, traceback):
+    log.d("TYPE: {}\nVALUE: {}\nTRACEBACK:\n\n{}".format(exctype, value, traceback))
+    sys.__excepthook__(exctype, value, traceback)
+sys.excepthook = my_except_hook
+
 
 # noinspection PyPep8Naming
 class Command:
--- leviot/ulog.py
+++ leviot/ulog.py
@@ -31,20 +31,33 @@ class Logger:
 
     def d(self, message):
         self._print_and_mqtt("DEBUG {}: {}".format(self.tag, message))
-        self.syslog_reconnect()
-        self.syslog.debug("{}: {}".format(self.tag, message))
+        try:
+            self.syslog_reconnect()
+            self.syslog.debug("{}: {}".format(self.tag, message))
+        except:
+            self.syslog = DummyClient()
 
     def i(self, message):
         self._print_and_mqtt("INFO  {}: {}".format(self.tag, message))
-        self.syslog_reconnect()
-        self.syslog.info("{}: {}".format(self.tag, message))
+        try:
+            self.syslog_reconnect()
+            self.syslog.info("{}: {}".format(self.tag, message))
+        except:
+            self.syslog = DummyClient()
+
 
     def w(self, message):
         self._print_and_mqtt("WARN  {}: {}".format(self.tag, str(message)))
-        self.syslog_reconnect()
-        self.syslog.warning("{}: {}".format(self.tag, message))
+        try:
+            self.syslog_reconnect()
+            self.syslog.warning("{}: {}".format(self.tag, message))
+        except:
+            self.syslog = DummyClient()
 
     def e(self, message):
         self._print_and_mqtt("ERROR {}: {}".format(self.tag, str(message)))
-        self.syslog_reconnect()
-        self.syslog.error("{}: {}".format(self.tag, message))
+        try:
+            self.syslog_reconnect()
+            self.syslog.error("{}: {}".format(self.tag, message))
+        except:
+            self.syslog = DummyClient()
